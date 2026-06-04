d47c7fe489e24afd6ef684de6732e90db7a556e1
Cristofer <saga.cristo2@gmail.com>
2026-06-03 22:17:28 -0400

    instalando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  ///// swagger
+  const config = new DocumentBuilder()
+    .setTitle('Backend Api')
+    .setDescription('Api Rest')
+    .setVersion('1.0')
+    .addTag('backend')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+  ////
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
